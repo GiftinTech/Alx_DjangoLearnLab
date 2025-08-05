5075e3c37275d9a4c399490bab0024bfb8857f8f
Gift Egbonyi <egbonyigiftvicky@gmail.com>
2025-08-05 13:51:31 +0100

    include app.url in advanced_api_project/url.py.

--- advanced-api-project/api/urls.py
+++ advanced-api-project/api/urls.py
@@ -1,6 +1,5 @@
 from django.urls import path
 from .views import (
-  BookGenericAPIView,
   BookListView,
   BookDetailView,
   BookCreateView,
@@ -9,7 +8,6 @@ from .views import (
 )
 
 urlpatterns = [
-  path('', BookGenericAPIView.as_view(), name='book-generic-api'),
   path('books/list/', BookListView.as_view(), name='book-list'),
   path('books/<int:pk>/', BookDetailView.as_view(), name='book-detail'),
   path('books/create/', BookCreateView.as_view(), name='book-create'),
