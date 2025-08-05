ec7349a8e4e7b392fc90d294c7045f2103a73000
Gift Egbonyi <egbonyigiftvicky@gmail.com>
2025-08-05 13:48:47 +0100

    Add BookGenericAPIView to url for autochecker.

--- advanced-api-project/api/urls.py
+++ advanced-api-project/api/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from .views import (
+  BookGenericAPIView,
   BookListView,
   BookDetailView,
   BookCreateView,
@@ -8,6 +9,7 @@ from .views import (
 )
 
 urlpatterns = [
+  path('', BookGenericAPIView.as_view(), name='book-generic-api'),
   path('books/list/', BookListView.as_view(), name='book-list'),
   path('books/<int:pk>/', BookDetailView.as_view(), name='book-detail'),
   path('books/create/', BookCreateView.as_view(), name='book-create'),
