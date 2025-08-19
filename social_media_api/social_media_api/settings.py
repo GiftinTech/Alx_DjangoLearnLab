c893f801d7e14ae5db9fc2a93f8c5ff27e35d571
Gift Egbonyi <egbonyigiftvicky@gmail.com>
2025-08-19 10:09:51 +0100

    And envs for Render.

--- social_media_api/social_media_api/settings.py
+++ social_media_api/social_media_api/settings.py
@@ -31,7 +31,7 @@ STATIC_ROOT = BASE_DIR / "staticfiles"
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-c-711)s3d&$l!vrf2z!@$nx-)%q!ks=u(_o-ftjpm_3*((g-&a'
+SECRET_KEY=os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
